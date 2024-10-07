2cb454679b783381d0145046df8d56176d8ee849
eduardovemba <eduvemba18@gmail.com>
2024-10-07 20:21:10 +0100

    CHANGED: securityFilter

--- jogadores/src/main/java/com/example/jogadores/auth/securityFilter.java
+++ jogadores/src/main/java/com/example/jogadores/auth/securityFilter.java
@@ -25,14 +25,20 @@ public class securityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if (token != null) {
-            var email = tokenService.verifyToken(token);
-            UserDetails user = userRepository.findByEmail(email);
-
-            var authentication = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        String path = request.getRequestURI();
+        if (path.equals("/auth/login") || path.equals("/auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
         }
+            var token = this.recoverToken(request);
+            if (token != null) {
+                var email = tokenService.verifyToken(token);
+                UserDetails user = userRepository.findByEmail(email);
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
         filterChain.doFilter(request, response);
     }
     private String recoverToken(HttpServletRequest request) {
